36b6c58a45694e9694540c3cefbc60fbd76b55c1
F.Ogata <ogataogara@gmail.com>
2021-07-12 21:00:16 -0300

    Exercicio inicial

---
+++ miniprojeto1.js
@@ -0,0 +1,44 @@
+let a = 'vermelho';
+let b = 'azul';
+
+let c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
+
+let x = 2;
+let y = 3;
+
+const z = x
+
+x = y;
+y = z;
+
+console.log(x);
+console.log(y);
+
+
+
+// const c = a
+// a = b
+
+// console.log(a);
+// console.log(c);s
+
+// Basicamente esse exercicio funciona para testar possibilidades de troca de variavel
+
+let something = 23
+function transformavariavel() { 
+    if(something === 32) { 
+        let something = 32 }
+    else 
+    something === 0;
+    console.log('transformavariavel')
+    return transformavariavel
+
+
+}
+
+transformavariavel();
